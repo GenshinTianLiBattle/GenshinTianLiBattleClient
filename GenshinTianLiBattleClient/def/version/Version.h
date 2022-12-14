fd1d2e02fe923d72379054935aa73b7a4db5786a
GengGode <GengGode@outlook.com>
2022-12-15 03:56:31 +0800

    chore:修复无法更新api版本问题

--- GenshinTianLiBattleClient/def/version/Version.h
+++ GenshinTianLiBattleClient/def/version/Version.h
@@ -4,19 +4,19 @@ namespace tl::battle::client::version
 #ifdef _DEBUG
 static const int version_major = 0;
 static const int version_minor = 2;
-static const int version_patch = 22;
-static const int version_build = 11;
-static const char* version = "0.2.22";
-static const char* version_hash = "5ed714b";
-static const char* build_version = "0.2.22.11-debug-5ed714b";
+static const int version_patch = 23;
+static const int version_build = 12;
+static const char* version = "0.2.23";
+static const char* version_hash = "0a9a1a2";
+static const char* build_version = "0.2.23.12-debug-0a9a1a2";
 #else
 static const int version_major = 0;
 static const int version_minor = 2;
-static const int version_patch = 22;
-static const int version_build = 11;
-static const char* version = "0.2.22";
-static const char* version_hash = "5ed714b";
-static const char* build_version = "0.2.22.11-master-5ed714b";
+static const int version_patch = 23;
+static const int version_build = 12;
+static const char* version = "0.2.23";
+static const char* version_hash = "0a9a1a2";
+static const char* build_version = "0.2.23.12-master-0a9a1a2";
 #endif
 }
 //该文件自动生成，无需更改 
