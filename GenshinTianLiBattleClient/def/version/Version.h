68ad360f604f52a9fdbdd63433f4f8a6815b8ef6
GengGode <GengGode@outlook.com>
2022-12-15 15:57:54 +0800

    update:更新Api

--- GenshinTianLiBattleClient/def/version/Version.h
+++ GenshinTianLiBattleClient/def/version/Version.h
@@ -4,19 +4,19 @@ namespace tl::battle::client::version
 #ifdef _DEBUG
 static const int version_major = 0;
 static const int version_minor = 2;
-static const int version_patch = 23;
-static const int version_build = 12;
-static const char* version = "0.2.23";
-static const char* version_hash = "0a9a1a2";
-static const char* build_version = "0.2.23.12-debug-0a9a1a2";
+static const int version_patch = 25;
+static const int version_build = 13;
+static const char* version = "0.2.25";
+static const char* version_hash = "ef4ebed";
+static const char* build_version = "0.2.25.13-debug-ef4ebed";
 #else
 static const int version_major = 0;
 static const int version_minor = 2;
-static const int version_patch = 23;
-static const int version_build = 12;
-static const char* version = "0.2.23";
-static const char* version_hash = "0a9a1a2";
-static const char* build_version = "0.2.23.12-master-0a9a1a2";
+static const int version_patch = 25;
+static const int version_build = 13;
+static const char* version = "0.2.25";
+static const char* version_hash = "ef4ebed";
+static const char* build_version = "0.2.25.13-master-ef4ebed";
 #endif
 }
 //该文件自动生成，无需更改 
