48a5534747bb3634ee9f8b67767d77643a816531
GengGode <GengGode@outlook.com>
2022-12-20 16:20:58 +0800

    update:更新corer的ocr

--- GenshinTianLiBattleClient/def/version/Version.h
+++ GenshinTianLiBattleClient/def/version/Version.h
@@ -4,19 +4,19 @@ namespace tl::battle::client::version
 #ifdef _DEBUG
 static const int version_major = 0;
 static const int version_minor = 2;
-static const int version_patch = 54;
-static const int version_build = 42;
-static const char* version = "0.2.54";
-static const char* version_hash = "d873864";
-static const char* build_version = "0.2.54.42-debug-d873864";
+static const int version_patch = 77;
+static const int version_build = 64;
+static const char* version = "0.2.77";
+static const char* version_hash = "8856aeb";
+static const char* build_version = "0.2.77.64-debug-8856aeb";
 #else
 static const int version_major = 0;
 static const int version_minor = 2;
-static const int version_patch = 54;
-static const int version_build = 42;
-static const char* version = "0.2.54";
-static const char* version_hash = "d873864";
-static const char* build_version = "0.2.54.42-master-d873864";
+static const int version_patch = 77;
+static const int version_build = 64;
+static const char* version = "0.2.77";
+static const char* version_hash = "8856aeb";
+static const char* build_version = "0.2.77.64-master-8856aeb";
 #endif
 }
 //该文件自动生成，无需更改 
