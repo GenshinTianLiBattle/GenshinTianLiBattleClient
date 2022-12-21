d9554a0d7b92ef15eb6ca2bf19059b3fa8af661a
GengGode <GengGode@outlook.com>
2022-12-21 14:20:00 +0800

    fix:修复链接libocr时未声明为静态库的错误

--- GenshinTianLiBattleClient/def/version/Version.h
+++ GenshinTianLiBattleClient/def/version/Version.h
@@ -4,19 +4,19 @@ namespace tl::battle::client::version
 #ifdef _DEBUG
 static const int version_major = 0;
 static const int version_minor = 2;
-static const int version_patch = 77;
-static const int version_build = 64;
-static const char* version = "0.2.77";
-static const char* version_hash = "8856aeb";
-static const char* build_version = "0.2.77.64-debug-8856aeb";
+static const int version_patch = 83;
+static const int version_build = 70;
+static const char* version = "0.2.83";
+static const char* version_hash = "48a5534";
+static const char* build_version = "0.2.83.70-debug-48a5534";
 #else
 static const int version_major = 0;
 static const int version_minor = 2;
-static const int version_patch = 77;
-static const int version_build = 64;
-static const char* version = "0.2.77";
-static const char* version_hash = "8856aeb";
-static const char* build_version = "0.2.77.64-master-8856aeb";
+static const int version_patch = 83;
+static const int version_build = 70;
+static const char* version = "0.2.83";
+static const char* version_hash = "48a5534";
+static const char* build_version = "0.2.83.70-master-48a5534";
 #endif
 }
 //该文件自动生成，无需更改 
